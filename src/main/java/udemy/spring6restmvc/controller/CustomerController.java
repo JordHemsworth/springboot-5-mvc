dfa7a9519bc99cee092f98d1c3e9d7a55aa5d15e
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2025-06-04 16:34:53 +0100

    Create Customer endpoints and service for listing all customers and list by ID

---
+++ src/main/java/udemy/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package udemy.spring6restmvc.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import udemy.spring6restmvc.model.Customer;
+import udemy.spring6restmvc.service.CustomerService;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return customerService.getCustomerById(customerId);
+    }
+
+
+
+}
