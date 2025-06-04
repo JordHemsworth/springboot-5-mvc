08f3109d54280a9f504ce22e13e0a04f9703a5df
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2025-06-04 16:04:02 +0100

    Add mapping to findBeerByID

--- src/main/java/udemy/spring6restmvc/controller/BeerController.java
+++ src/main/java/udemy/spring6restmvc/controller/BeerController.java
@@ -2,10 +2,11 @@ package udemy.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import udemy.spring6restmvc.model.Beer;
-import org.springframework.stereotype.Controller;
 import udemy.spring6restmvc.service.BeerService;
 
 import java.util.List;
@@ -14,19 +15,21 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer ID in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 
 }
