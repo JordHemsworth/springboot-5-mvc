2d5708fdbcaa4bfe186f8118111ab9f07dac0f58
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2025-06-04 14:24:38 +0100

    Added logging from Section 5 / 56.
    java: cannot find symbol
      symbol:   variable log
      location: class udemy.spring6restmvc.controller.BeerController

---
+++ src/main/java/udemy/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,24 @@
+package udemy.spring6restmvc.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import udemy.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Controller;
+import udemy.spring6restmvc.service.BeerService;
+
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+
+    public Beer getBeerById(UUID id){
+
+        log.debug("Get Beer ID in service was called");
+
+        return beerService.getBeerById(id);
+    }
+
+}
--- src/main/java/udemy/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/udemy/spring6restmvc/service/BeerServiceImpl.java
@@ -1,15 +1,22 @@
-package service;
+package udemy.spring6restmvc.service;
 
-import model.Beer;
-import model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import udemy.spring6restmvc.model.Beer;
+import udemy.spring6restmvc.model.BeerStyle;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer ID in service was called");
+
         return Beer.builder()
                 .id(id)
                 .version(1)
