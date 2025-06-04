2d6dade7ecd1a0b2e36efa844b015438673a505c
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2025-06-04 15:50:22 +0100

    Move to RestController to return response body, provide mapping to api endpoint

--- src/main/java/udemy/spring6restmvc/controller/BeerController.java
+++ src/main/java/udemy/spring6restmvc/controller/BeerController.java
@@ -2,17 +2,25 @@ package udemy.spring6restmvc.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import udemy.spring6restmvc.model.Beer;
 import org.springframework.stereotype.Controller;
 import udemy.spring6restmvc.service.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id){
 
