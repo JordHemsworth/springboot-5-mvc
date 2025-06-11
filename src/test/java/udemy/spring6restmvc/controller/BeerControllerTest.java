225078f0687c9739e06ba99a369140c4d310583a
JordHemsworth <88255447+JordHemsworth@users.noreply.github.com>
2025-06-11 11:01:49 +0100

    Move BeerTest to MockMvc
    Add to SpringContext to allow autowire

--- src/test/java/udemy/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/udemy/spring6restmvc/controller/BeerControllerTest.java
@@ -2,20 +2,33 @@ package udemy.spring6restmvc.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import udemy.spring6restmvc.service.BeerService;
 
 import java.util.UUID;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockitoBean
+    BeerService beerService;
 
     @Test
-    void getBeerById() {
+    void getBeerById() throws Exception {
 
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
-
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())          //Get random beer by ID and should return 200
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
